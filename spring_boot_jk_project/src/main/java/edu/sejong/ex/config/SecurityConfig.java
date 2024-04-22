edf6ee961051d907c4f1031058613752143026ab
DESKTOP-3NSI7NG\jk <mobin0625@protonmail.com>
2024-04-22 00:18:18 -0700

    step3 generate user and password

--- spring_boot_jk_project/src/main/java/edu/sejong/ex/config/SecurityConfig.java
+++ spring_boot_jk_project/src/main/java/edu/sejong/ex/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package edu.sejong.ex.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,12 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 		// web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
-
 	}
-
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("member").password("{noop}member").roles("USER")
+			.and()
+			.withUser("admin").password("{noop}admin").roles("ADMIN")
+		;
+	}
 }
